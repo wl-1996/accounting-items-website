0c9c5a93a6d35cfee43a8fa7af14b2b8650b4601
WK <741341649@qq.com>
2020-05-03 11:08:54 +0800

    deploy

---
+++ precache-manifest.c814148b47018ad9daa10f3e6fd3b2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c3f52077232122aadabaf24621f791",
+    "url": "/accounting-items-website/QR-code.png"
+  },
+  {
+    "revision": "230e01f98bca025bce01",
+    "url": "/accounting-items-website/css/app.617a2c7d.css"
+  },
+  {
+    "revision": "95e128e51286f23fd6c5f70240529986",
+    "url": "/accounting-items-website/index.html"
+  },
+  {
+    "revision": "230e01f98bca025bce01",
+    "url": "/accounting-items-website/js/app.17d48114.js"
+  },
+  {
+    "revision": "9bbf71e9c97e4ada9ff9",
+    "url": "/accounting-items-website/js/chunk-vendors.2348d4b6.js"
+  },
+  {
+    "revision": "a4d9dab9799dcb3088130532ac9d72d5",
+    "url": "/accounting-items-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-items-website/robots.txt"
+  }
+]);
